d5258e5c3457cac1d0048f5e93a4569f7a6150a4
Mildred-Herba <mildredherba53@gmail.com>
2025-12-17 03:16:56 +0800

    db

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL="postgresql://neondb_owner:npg_ZxsQ4IA3SfqW@ep-square-water-adoe2auv-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false } // required for Neon
 });
 
